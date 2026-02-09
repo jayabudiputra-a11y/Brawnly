6b0e2efd012a40465bfd9e91ba88b8fba5b0f585
Budi Putra Jaya <jayabudiputra@gmail.com>
2026-02-09 16:25:02 +0700

    tambah 1

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const { SitemapStream, streamToPromise } = require('sitemap');
-const { Readable } = require('stream');
+import fs from 'fs';
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { Readable } from 'stream';
 
 async function generateSitemap() {
   const links = [
@@ -8,11 +8,13 @@ async function generateSitemap() {
     { url: '/articles', changefreq: 'daily', priority: 0.8 },
     { url: '/about', changefreq: 'monthly', priority: 0.5 },
     { url: '/contact', changefreq: 'monthly', priority: 0.5 },
+    { url: '/terms', changefreq: 'monthly', priority: 0.3 },
+    { url: '/privacy', changefreq: 'monthly', priority: 0.3 },
+    { url: '/ethics', changefreq: 'monthly', priority: 0.3 },
   ];
 
   try {
     const stream = new SitemapStream({ hostname: 'https://www.brawnly.online' });
-    
     const xml = await streamToPromise(Readable.from(links).pipe(stream)).then((data) =>
       data.toString()
     );
